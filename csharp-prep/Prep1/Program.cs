4b10db686e9eadc887a08b23f9c16072239e11f7
warndahlo <warndahlo@gmail.com>
2025-01-09 16:54:37 -0700

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+
+        Console.WriteLine($"your name is {last}, {first} {last}");
     }
 }
